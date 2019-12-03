3b2cbc72b7ea4f8e4aa07b395a6540a12245d9ae
Zbigniew Tomanek <zbigniew.tomanek@protonmail.com>
2019-12-03 14:14:09 +0100

    Added track drawning feature

--- main.py
+++ main.py
@@ -8,15 +8,21 @@ Created on Tue Dec  3 10:17:02 2019
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import (
-        NumericProperty, ReferenceListProperty, ObjectProperty
+        NumericProperty, ReferenceListProperty, ObjectProperty, BooleanProperty
         )
 from kivy.vector import Vector
 from kivy.clock import Clock
 from math import sin, cos, radians
 from kivy.core.window import Window
+from kivy.graphics import Color, Ellipse, Line
+
+import imageio
+
+MAP_FILENAME = 'track.png'
 
 
 class Car(Widget):
+    visible = BooleanProperty(True)
     angle = NumericProperty(90)
     vx = NumericProperty(0)
     vy = NumericProperty(0)
@@ -34,26 +40,46 @@ class Car(Widget):
         
 class CarSimulation(Widget):
     car = ObjectProperty(None)
+    bitmap = None
     
     def __init__(self, **kwargs):
         super(CarSimulation, self).__init__(**kwargs)
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
         
+    def on_touch_down(self, touch):
+        with self.canvas:
+            Color(1, 1, 1)
+            touch.ud['line'] = Line(points=(touch.x, touch.y))
+            
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += [touch.x, touch.y]
+        
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
+        
+    def load_bitmap(self):
+        self.car.visible = False
+        self.export_to_png(MAP_FILENAME)
+        self.car.visible = True
+        self.bitmap = imageio.imread(MAP_FILENAME)
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        print(self.car.vx, self.car.vy)
-        if keycode[1] == 'w':
-            self.car.move(1)
-        elif keycode[1] == 's':
-            self.car.move(-1)
-        elif keycode[1] == 'd':
-            self.car.turn(-5)
-        elif keycode[1] == 'a':
-            self.car.turn(5)
+        c = keycode[1]
+
+        if self.bitmap is None:
+            if c == 'spacebar':
+                self.load_bitmap()
+        else:
+            if c == 'w':
+                self.car.move(1)
+            elif c == 's':
+                self.car.move(-1)
+            elif c == 'd':
+                self.car.turn(-5)
+            elif c == 'a':
+                self.car.turn(5)
         return True
     
     def init(self):
@@ -64,8 +90,7 @@ class CarSimulation(Widget):
     def update(self, dt):
    
         self.car.move(0)
-
-
+        
         if (self.car.y < 0) or (self.car.top > self.height):
             self.car.vy = 0
             self.car.vx = 0
