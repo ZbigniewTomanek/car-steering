508d3265eee7c989cb786e10746dcb92690eafdd
Zbigniew <zbigniew.tomanek@protonmail.com>
2019-12-12 12:39:06 +0100

    Added requirements file and reformated code

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ Created on Tue Dec  3 10:17:02 2019
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import (
-        NumericProperty, ReferenceListProperty, ObjectProperty, BooleanProperty
-        )
+    NumericProperty, ReferenceListProperty, ObjectProperty, BooleanProperty
+)
 from kivy.vector import Vector
 from kivy.clock import Clock
 from math import sin, cos, radians
@@ -28,106 +28,100 @@ class Car(Widget):
     vx = NumericProperty(0)
     vy = NumericProperty(0)
     velocity = ReferenceListProperty(vx, vy)
-    
+
     def turn(self, angle):
         self.vx, self.vy = Vector(*self.velocity).rotate(angle)
         self.angle += round(angle)
         self.move()
-        
+
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
-        
+
     def accelerate(self, delta):
         self.vx += cos(radians(self.angle)) * delta
         self.vy += sin(radians(self.angle)) * delta
         self.move()
-        
+
+
 class TrackPainter(Widget):
     lines = []
-    
+
     def on_touch_down(self, touch):
         with self.canvas:
             Color(1, 1, 0)
             touch.ud['line'] = Line(points=(touch.x, touch.y))
-                
+
     def draw_points(self, points):
-        self.points = points
         self.canvas.clear()
-        
+
         with self.canvas:
-                Color(1, 1, 0)
-                for l in self.lines:
-                    Line(points=l)
-                
-                Color(1, 0, 0)
-                for p in points:
-                    Ellipse(pos=(p[0] - 2.5, p[1] - 2.5), size=(5, 5))
-                
-                
-            
+            Color(1, 1, 0)
+            for l in self.lines:
+                Line(points=l)
+
+            Color(1, 0, 0)
+            for p in points:
+                Ellipse(pos=(p[0] - 2.5, p[1] - 2.5), size=(5, 5))
+
     def on_touch_move(self, touch):
         touch.ud['line'].points += [touch.x, touch.y]
-            
-            
+
     def on_touch_up(self, touch):
         self.lines.append(touch.ud['line'].points)
-        
+
     def load_bitmap(self):
         self.canvas.clear()
         with self.canvas:
             Color(1, 1, 0)
             for l in self.lines:
                 Line(points=l)
-                    
+
         self.export_to_png(MAP_FILENAME)
-        return imageio.imread(MAP_FILENAME)[:,:,0]
-    
+        return imageio.imread(MAP_FILENAME)[:, :, 0]
+
     def clear(self):
         self.lines.clear()
         self.canvas.clear()
-    
-        
+
+
 class CarSimulation(Widget):
     car = ObjectProperty(None)
-    
+
     speed = 1
     bitmap = None
-    
+
     p = Plotter()
     controller = ObjectProperty(None)
     pid = BooleanProperty(False)
     K = -0.01
-    
+
     kp = NumericProperty(0)
     ki = NumericProperty(0)
     kd = NumericProperty(0)
-        
-    
+
     def __init__(self, **kwargs):
         super(CarSimulation, self).__init__(**kwargs)
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
-        
+
         Window.size = WINDOW_SIZE
-        
+
         self.tpainter = TrackPainter(size=WINDOW_SIZE)
         self.add_widget(self.tpainter)
-                
-        
+
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
-        
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
         c = keycode[1]
-        
-        if not self.pid: # manual mode
+
+        if not self.pid:  # manual mode
             if c == 'd':
                 err = collide_with_track(self.bitmap, self.car)
                 self.car.turn(-5)
                 self.p.refresh(err, -5)
-            elif c == 'a': 
+            elif c == 'a':
                 err = collide_with_track(self.bitmap, self.car)
                 self.car.turn(5)
                 self.p.refresh(err, 5)
@@ -136,59 +130,58 @@ class CarSimulation(Widget):
                 self.init_pid(-45, 45)
                 self.pid = True
                 return True
-            
-                
+
         if c == 'w':
             self.car.accelerate(self.speed)
         elif c == 's':
             self.car.accelerate(-self.speed)
         elif c == 'k':
-             print('manual mode')
-             self.pid = False
-             self.controller = ObjectProperty(None)
+            print('manual mode')
+            self.pid = False
+            self.controller = ObjectProperty(None)
         elif c == 'z':
-            self.set_pid(self.K*1.2)
+            self.set_pid(self.K * 1.2)
         elif c == 'x':
-            self.set_pid(self.K/1.2)
+            self.set_pid(self.K / 1.2)
         if c == 'p':
             self.p.plot()
-            Window.size = WINDOW_SIZE #to stop resizing caused by matplotlib
+            Window.size = WINDOW_SIZE  # to stop resizing caused by matplotlib
         elif c == 'spacebar':
             self.car.center = self.center
             self.car.vx = 0
             self.car.vy = 0
         elif c == 'backspace':
-                self.tpainter.clear()
-                self.bitmap = self.tpainter.load_bitmap()
-    
+            self.tpainter.clear()
+            self.bitmap = self.tpainter.load_bitmap()
+
         return True
-    
+
     def on_touch_up(self, touch):
         self.tpainter.on_touch_up(touch)
         self.bitmap = self.tpainter.load_bitmap()
-        
+
     def on_touch_down(self, touch):
         self.tpainter.on_touch_down(touch)
-    
+
     def on_touch_move(self, touch):
         self.tpainter.on_touch_move(touch)
-    
+
     def set_pid(self, K):
         self.K = K
-        self.controller.set_tunings(1.2 * self.K, 0,0)
+        self.controller.set_tunings(1.2 * self.K, 0, 0)
         self.kp = self.controller.kp
         self.ki = self.controller.ki
         self.kd = self.controller.kd
-    
+
     def init_pid(self, o_min, o_max):
         self.controller = PID(1.2 * self.K, 0, 0, o_min, o_max)
-        
+
         self.kp = self.controller.kp
         self.ki = self.controller.ki
         self.kd = self.controller.kd
-        
+
         self.car.accelerate(self.speed)
-    
+
     def init(self):
         self.bitmap = self.tpainter.load_bitmap()
         self.car.center = self.center
@@ -201,7 +194,7 @@ class CarSimulation(Widget):
             coll = collide_with_track(self.bitmap, self.car)
             err, points = count_ray_error(self.bitmap, self.car)
             self.tpainter.draw_points(points)
-            
+
             dec = 0
             if self.pid:
                 if self.controller.is_ready():
@@ -210,15 +203,12 @@ class CarSimulation(Widget):
                         print(dec, err)
                     self.car.turn(dec)
             else:
-                if coll: # if car is in manual mode stop the car on collision
+                if coll:  # if car is in manual mode stop the car on collision
                     self.car.vy = 0
                     self.car.vx = 0
-                
-                
+
             self.p.refresh(err, dec)
-            
-           
-                
+
         if (self.car.y < 0) or (self.car.top > self.height):
             self.car.vy = 0
             self.car.vx = 0
@@ -226,16 +216,16 @@ class CarSimulation(Widget):
         if (self.car.x < 0) or (self.car.right > self.width):
             self.car.vy = 0
             self.car.vx = 0
-            
+
 
 class CarApp(App):
     def build(self):
         sim = CarSimulation()
         sim.init()
-        Clock.schedule_interval(sim.update, 1.0/60.0)
-        
- 
+        Clock.schedule_interval(sim.update, 1.0 / 60.0)
+
         return sim
-    
+
+
 if __name__ == '__main__':
     CarApp().run()
--- steering.py
+++ steering.py
@@ -10,65 +10,50 @@ from matplotlib import pyplot as plt
 import time
 import math
 
+
 def is_point_on_right(x1, x2, xA, y1, y2, yA):
-    v1 = (x2-x1, y2-y1)
-    v2 = (x2-xA, y2-yA)
-    xp = v1[0]*v2[1] - v1[1]*v2[0]
-    
+    v1 = (x2 - x1, y2 - y1)
+    v2 = (x2 - xA, y2 - yA)
+    xp = v1[0] * v2[1] - v1[1] * v2[0]
+
     return xp > 0
 
-def collide_with_track(layer, car):
 
+def collide_with_track(layer, car):
     cx, cy = car.center
     w, h = car.size
     angle = car.angle
-    
-    v = Vector(-w/2, h/2).rotate(angle)
-    blc = cx + v.x, cy + v.y # bottom left corner
-    
-    
+
+    v = Vector(-w / 2, h / 2).rotate(angle)
+    blc = cx + v.x, cy + v.y  # bottom left corner
+
     v1 = Vector(0, -h).rotate(angle)
     v2 = Vector(w, -h).rotate(angle)
     v3 = Vector(w, 0).rotate(angle)
-    
+
     vectors = [v1, v2, v3]
     x = [blc[0] + _v.x for _v in vectors]
     y = [blc[1] + _v.y for _v in vectors]
     x.append(blc[0])
     y.append(blc[1])
-   
+
     # count bbmax 
     x_min = int(min(x))
     x_max = int(max(x))
     y_min = int(min(y))
     y_max = int(max(y))
-    
+
     lh, lw = layer.shape
-    
-    #err = 0
-    #vc = Vector(0, 1).rotate(angle) #vector fo cheking side of point towards center
-    #vcp = Vector(-1, 0).rotate(angle)
-    
-    for i in range(x_min, x_max+1):
-        for j in range(y_min, y_max+1):
+
+    for i in range(x_min, x_max + 1):
+        for j in range(y_min, y_max + 1):
             for k in range(4):
-               if not is_point_on_right(x[k], x[(k+1)%4], i, y[k], y[(k+1)%4], j):
-                   continue
+                if not is_point_on_right(x[k], x[(k + 1) % 4], i, y[k], y[(k + 1) % 4], j):
+                    continue
             if i < lw and 0 < j < lh:
-                if layer[lh-j][i] > 0:
+                if layer[lh - j][i] > 0:
                     return True
-                    
-                    #if is_point_on_right(cx, cx - vcp.x, i, cy, cy - vcp.y, j):
-                        #print('Z przodu!')
-                        #if is_point_on_right(cx, cx-vc.x, i, cy, cy-vc.y, j):
-                            #print('prawo')
-                            #err -= 1
-                        #else:
-                            #print('lewo')
-                            #err += 1
-                    #else:
-                        #print('z tylu')
-                       
+
     return False
 
 
@@ -77,65 +62,62 @@ def count_ray_error(layer, car, max_distance=40, angle_delta=20):
     cw, ch = car.size
     lh, lw = layer.shape
     err = 0
-    
-    min_d = round(math.sqrt((cw/2)**2 + (ch/2)**2)) # dystans od którego będzie sprawdzany
+
+    min_d = round(math.sqrt((cw / 2) ** 2 + (ch / 2) ** 2))  # dystans od którego będzie sprawdzany
     sv = Vector(0, -1).rotate(car.angle)
-    
+
     points_x = []
     points_y = []
-    
+
     for angle in range(0, 181, angle_delta):
-        #shift center outside a car
+        # shift center outside a car
         sx = cx + sv.x * min_d
         sy = cy + sv.y * min_d
         dis = max_distance
-        
+
         found = False
         for d in range(0, max_distance):
             py = int(sy + sv.y * d)
             px = int(sx + sv.x * d)
-            
-            if px >= lw or px < 0 or lh-py <= 0 or lh-py >= lh:
+
+            if px >= lw or px < 0 or lh - py <= 0 or lh - py >= lh:
                 points_x.append(px)
                 points_y.append(py)
                 found = True
                 dis = d
                 break
-                
-            if layer[lh-py][px] > 0:
+
+            if layer[lh - py][px] > 0:
                 # ray found nearest point
                 points_x.append(px)
                 points_y.append(py)
                 found = True
                 dis = d
                 break
-        
+
         if not found:
             points_x.append(sx + sv.x * max_distance)
             points_y.append(sy + sv.y * max_distance)
-            
+
         sv = sv.rotate(angle_delta)
-        
+
         if angle < 90:
-            err += (dis - max_distance)**2
+            err += (dis - max_distance) ** 2
         else:
-            err -= (dis - max_distance)**2
-            
+            err -= (dis - max_distance) ** 2
+
     return err, list(zip(points_x, points_y))
-                
-            
-    
 
 
 class Plotter:
     t = 0
     a_err = 0
-    
+
     time = []
     steering = []
     error = []
     err = []
-    
+
     def refresh(self, err_t, steer):
         self.a_err += err_t
         self.time.append(self.t)
@@ -143,74 +125,72 @@ class Plotter:
         self.err.append(err_t)
         self.error.append(self.a_err)
         self.steering.append(steer)
-        
-    
+
     def plot(self):
         plt.clf()
         ax = plt.subplot(311)
-        
+
         ax.set_title('Sterowanie')
         ax.plot(self.time, self.steering, color='g')
-    
+
         ax = plt.subplot(312)
         ax.set_title('Blad chwilowy')
         ax.plot(self.time, self.err, color='r')
-    
+
         ax = plt.subplot(313)
         ax.set_title('Blad calkowity')
         ax.plot(self.time, self.error)
-    
+
         plt.pause(0.01)
-    
+
 
 class PID:
     integral_sum = 0
-    
+
     sample_time = 0
     setpoint = 0
     last_time = 0
     last_input = 0
-    
+
     o_min = 0
     o_max = 0
-    
+
     def __init__(self, kp, ki, kd, out_min, out_max, sample=0.1):
         self.sample_time = sample
-        
+
         if out_min > out_max:
             raise ValueError
-            
+
         self.o_min = out_min
         self.o_max = out_max
         self.set_tunings(kp, ki, kd)
-        
+
     def set_tunings(self, kp, ki, kd):
         sample_in_sec = self.sample_time / 1000
         self.kp = kp
         self.ki = ki * sample_in_sec
         self.kd = kd / sample_in_sec
-        
-        
+
     def is_ready(self):
         return time.time() - self.last_time >= self.sample_time
-    
+
     def steer(self, inpt):
         err = self.setpoint - inpt
-        
+
         self.integral_sum += self.ki * err
         if self.integral_sum > self.o_max:
             self.integral_sum = self.o_max
-        
+
         d_input = inpt - self.last_input
-        
+
         output = self.kp * err + self.integral_sum + self.kd * d_input
-        
+
         if output > self.o_max:
             output = self.o_max
         elif output < self.o_min:
             output = self.o_min
-        
+
         self.last_time = time.time()
         self.last_input = inpt
-        
+
         return output
